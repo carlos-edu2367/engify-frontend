2ce3d7db695597a5d04dd29cc27eaceb37de6ea2
unknown <contato.carlosedu2367@gmail.com>
2026-04-29 10:58:16 -0300

    a

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -76,6 +76,8 @@ export const useAuthStore = create<AuthState>()(
       },
       partialize: (state) => ({
         user: state.user,
+        accessToken: state.accessToken,
+        isAuthenticated: state.isAuthenticated,
       }),
     }
   )
